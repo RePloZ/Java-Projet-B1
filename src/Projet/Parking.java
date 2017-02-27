e6bb537350e2881af563cc8cd99055df070eb1f3
RePloZ <herve.kay@ynov.com>
2017-02-27 10:36:29 +0100

    add var +gettersetter

--- src/Projet/Parking.java
+++ src/Projet/Parking.java
@@ -1,8 +1,16 @@
 package Projet;
 
-import com.sun.javaws.ui.CacheViewDialog;
-
 public class Parking {
+    private Object parking[][] = new Object[22][22];
+    private int placeRestante = 0;
 
-
+    public void setParking(Object[][] parking) {
+        this.parking = parking;
+    }
+    public int getPlaceRestante() {
+        return placeRestante;
+    }
+    public void setPlaceRestante(int placeRestante) {
+        this.placeRestante = placeRestante;
+    }
 }
--- src/Projet/Voiture.java
+++ src/Projet/Voiture.java
@@ -1,5 +1,43 @@
 package Projet;
 
-public class Voiture extends Parking {
+import java.util.Date;
+
+public class Voiture {
+    private String nameCar;
+    private String marqueCar;
+    private String immatriculationCar;
+    private Date entree;
+    private Date sortie;
+    private int positionX;
+    private int positionY;
+
+    public Voiture(String pNameCar, String pMarqueCar, String pImmatriculationCar, int pPositionX, int pPositionY){
+        this.nameCar = pNameCar;
+        this.marqueCar = pMarqueCar;
+        this.entree = new Date();
+        this.positionX = pPositionX;
+        this.positionY = pPositionY;
+    }
+
+    public Date getEntree() {
+        return entree;
+    }
+    public Date getSortie() {
+        return sortie;
+    }
+    public String getImmatriculationCar() {
+        return immatriculationCar;
+    }
+    public int getPositionX() {
+        return positionX;
+    }
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setSortie(Date sortie) {
+        this.sortie = sortie;
+    }
+
 
 }
