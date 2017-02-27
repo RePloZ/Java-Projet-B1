dfbb25b134721639412072042ad355383973fcfb
RePloZ <herve.kay@ynov.com>
2017-02-27 09:57:18 +0100

    rewok

--- src/Projet/Voiture.java
+++ src/Projet/Voiture.java
@@ -1,21 +1,5 @@
 package Projet;
 
 public class Voiture extends Parking {
-    String name;
-    int temps;
-    int tarif;
 
-    public void Voirue(String name, int temps) {
-        this.name = name;
-        this.temps = temps;
-    }
-    public void entrerParking(){
-        //Ajouter une voiture, vérifier si le parking est plain (on appelera la fonction parking plein)
-        this.placeOccupe++;
-        this.placeVide--;
-    }
-    public int payer(){
-      //Faire payer le client selon le temps où il est resté
-        return this.temps * this.tarif;
-    }
 }
