538e22033dfa6218f5ac161ec2e546ad56c3d0b1
RePloZ <herve.kay@ynov.com>
2017-03-02 16:26:47 +0100

    add calendar

---
+++ src/Projet/Horloge.java
@@ -0,0 +1,49 @@
+package Projet;
+import java.util.Calendar;
+
+public class Horloge {
+    Calendar MonCalendrier = new Calendar() {
+        @Override
+        protected void computeTime() {
+
+        }
+
+        @Override
+        protected void computeFields() {
+
+        }
+
+        @Override
+        public void add(int field, int amount) {
+
+        }
+
+        @Override
+        public void roll(int field, boolean up) {
+
+        }
+
+        @Override
+        public int getMinimum(int field) {
+            return 0;
+        }
+
+        @Override
+        public int getMaximum(int field) {
+            return 0;
+        }
+
+        @Override
+        public int getGreatestMinimum(int field) {
+            return 0;
+        }
+
+        @Override
+        public int getLeastMaximum(int field) {
+            return 0;
+        }
+
+        set
+    };
+    MonCalendrier.set(1, 1, 1, 0, 0);
+}
