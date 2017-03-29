e8d6192d7dd07819b2b323c5fd9571a3d8210b3e
Sarah <sarah.simon@ynov.com>
2017-03-29 13:49:49 +0200

    modif

--- src/Projet/Main.java
+++ src/Projet/Main.java
@@ -3,6 +3,27 @@ package Projet;
 public class Main {
 	public static void main(String[] args) {
 		Fenetre fen = new Fenetre();
+		/*boucle qui va se répéter tant que le jeu n'est pas terminer
+		Demander à l'utilisateur si il veut:
+		-rentrer une voiture dans le parking 
+		-faire sortir une voiture du parking
+		-quitter le programme*/
+		boolean restart  = true;
+		while (restart){
+			System.out.println("Demander à l'utilisateur si il veut:"+
+		"\n 1- Entrer une voiture dans le parking"+
+		"\n 2- Faire sortir une voiture du parking"+
+		"\n 3- Quitter le programme");
+		switch(){
+		case 1:
+			Voiture 
+			break;
+		case 2:
+			break;
+		case 3:
+			restart = false;
+			break;}
+		}
 	}
-
+	
 }
