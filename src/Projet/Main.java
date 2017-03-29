a5f582407f45c0b92de1f1a2ade8d99567e460c2
Sarah <sarah.simon@ynov.com>
2017-03-29 13:54:01 +0200

    modif

--- src/Projet/Main.java
+++ src/Projet/Main.java
@@ -4,33 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		Fenetre fen = new Fenetre();
-		/*boucle qui va se répéter tant que le jeu n'est pas terminer
-		Demander à l'utilisateur si il veut:
-		-rentrer une voiture dans le parking 
-		-faire sortir une voiture du parking
-		-quitter le programme*/
-		boolean restart  = true;
-		while (restart){
-			System.out.println("Demander à l'utilisateur si il veut:"+
-		"\n 1- Entrer une voiture dans le parking"+
-		"\n 2- Faire sortir une voiture du parking"+
-		"\n 3- Quitter le programme");
-		switch(){
-		case 1:
-			Voiture 
-			break;
-		case 2:
-			break;
-		case 3:
-			restart = false;
-			break;}
-		}
-	}
-	
-=======
+
     private ArrayList<String> NomVoiture = new ArrayList<>();
     public static void main(String[] args) {
         Fenetre fen = new Fenetre();
